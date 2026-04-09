ffbf794c819d757c4d977533ecb294cc03aab7ce
jotibiswas013-pixel <jotibiswas013@gmail.com>
2026-04-09 13:03:52 +0600

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/core/storage/framework/maintenance.php')) {
-    require $maintenance; hello world
+    require $maintenance;
 }
 
 // Register the Composer autoloader...
